8f1fd6b848d93e255473c18eafa0644fb8411f58
jc437351 <saggu.monty@gmail.com>
2016-12-16 21:30:58 +1000

    updated functions

--- Prac04/quick_pick.py
+++ Prac04/quick_pick.py
@@ -4,14 +4,15 @@ from random import randint
 
 num_of_picks= int(input("How many quick picks?: "))
 
-pick_of_6_num = []
+
 
 num= 0
 
 for i in range(0,num_of_picks):
     num= randint(1, 45)
+    pick_of_6_num = []
     for j in range(0,6):
-        while num is pick_of_6_num:
+        while num in pick_of_6_num:
             num= randint(1, 45)
         pick_of_6_num.append(num)
     pick_of_6_num.sort()
